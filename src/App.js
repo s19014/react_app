29c4f2333aa12fa3e703b6ffe7623a98cf2bbaee
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-12-17 14:55:42 +0900

    App.js 修正

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Hello = props => {
-  return <h1>Hello, world!</h1>
+const Greeting = props => {
+  const r = Math.floor(Math.random() * 3)
+  if (r == 0) return <p>今日も頑張りましょう</p>
+  if (r == 1) return <p>こんにちは。</p>
+  if (r == 2) return <p>朗らかな日ですね</p>
 }
+
 class App extends React.Component {
   constructor (props) {
     super(props)
@@ -11,7 +15,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <Hello />
+        <Greeting />
       </div>
     )
   }
