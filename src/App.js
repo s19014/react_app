d9161ec23587256daa2abe5487a37239f4c59b8d
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-12-17 17:57:34 +0900

    App.js 修正

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from 'react'
 
 const Maxim = props => {
-    return (
-        <div>
-            <p>探すのに時があり</p>
-            <p>捨てるのに時がある</p>
-        </div>
-    )
+  return (
+    <div>
+      <p style={css1}>探すのに時があり</p>
+      <p style={css2}>捨てるのに時がある</p>
+    </div>
+  )
 }
 
 const css1 = {
-    "color": 'red',
-    "background-color": '#f0f0ff',
-    "font-size": '2em'
+  color: 'red',
+  'background-color': '#f0f0ff',
+  'font-size': '2em'
+}
+
+const css2 = {
+  color: 'blue',
+  backgroundColor: '#fff0ff',
+  fontSize: '2em'
 }
 
 class App extends React.Component {
