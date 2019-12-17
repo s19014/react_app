97481ccea8b29a93618d46d09025500babf6c4f5
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-12-17 14:48:06 +0900

    App.js Hello,world!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from './react'
+import React from 'react'
 
+const Hello = props => {
+  return <h1>Hello, world!</h1>
+}
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {}
   }
   render () {
-    return <div />
+    return (
+      <div>
+        <Hello />
+      </div>
+    )
   }
 }
 
