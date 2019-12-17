2409a347c98b7df27574587ffeecd29f8536d4e2
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-12-17 16:14:06 +0900

    App.js 修正

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
+const Maxim = props => {
+  return (
+    <div>
+      <p>探すのに時があり</p>
+      <p>捨てるのに時がある</p>
+    </div>
+  )
+}
+
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {}
   }
   render () {
-    return <div />
+    return <Maxim />
   }
 }
 
