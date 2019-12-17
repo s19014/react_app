7458a8b3889806a769a7ebd5798df58c4b3bd2fa
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-12-17 14:24:09 +0900

    App.js さわった

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  render () {
+    return (
+      <div className='APP'>
+        <h2>塔を立てる時は、まず座って費用を計算しよう</h2>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
