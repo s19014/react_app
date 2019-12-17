d3cf0297f58160fc2020684e6d5994770ad71fea
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-12-17 16:09:39 +0900

    App.js 修正

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
+const TitleAndImg = props => {
+  const title = '書道'
+  const imgUrl = 'http://uta.pw/shodou/img/28/214.png'
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>
+        <img src={imgUrl} />
+      </p>
+    </div>
+  )
+}
+
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {}
   }
   render () {
-    return <div />
+    return <TitleAndImg />
   }
 }
 
