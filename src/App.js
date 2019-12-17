a4f04b8bdfb7cc3f3158bbcd2baea7c980356328
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-12-17 16:34:02 +0900

    App.js 修正　完成ではない

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
+const Maxim = props => {
+    return (
+        <div>
+            <p>探すのに時があり</p>
+            <p>捨てるのに時がある</p>
+        </div>
+    )
+}
+
+const css1 = {
+    "color": 'red',
+    "background-color": '#f0f0ff',
+    "font-size": '2em'
+}
+
 class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {}
   }
   render () {
-    return <div />
+    return <Maxim />
   }
 }
 
