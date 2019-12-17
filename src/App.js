21a6a5cd14be330db739e732ae5bd1f9e8d95f83
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-12-17 15:42:56 +0900

    App.js 修正2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const itemValue = props => {
+const ItemValue = props => {
   const item = 'SDカード'
   const value = 1200
   return (
@@ -18,7 +18,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <itemValue />
+        <ItemValue />
       </div>
     )
   }
