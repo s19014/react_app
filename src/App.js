83698ea9dfbac8112d0fd86dc9aa5c02ce2f6f70
Tedokon Ryota <s19014@std.it-college.ac.jp>
2019-12-19 16:32:36 +0900

    App.js 修正

--- src/App.js
+++ src/App.js
@@ -3,10 +3,37 @@ import React from 'react'
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {}
+    console.log('constructor')
   }
+
+  componentDidMount () {
+    console.log('componentDidMount')
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('shouldComponentUpdate')
+    return true
+  }
+
+  componentDidUpdate () {
+    console.log('componentDidUpdate')
+  }
+
+  componentWillUnmount () {
+    console.log('componentWillmount')
+  }
+
   render () {
-    return <div />
+    console.log('render')
+    const setStateHandler = e => {
+      console.log('* call setState()')
+      this.setState({ r: Math.random() })
+    }
+    return (
+      <div>
+        <button onClick={setStateHandler}>setState</button>
+      </div>
+    )
   }
 }
 
